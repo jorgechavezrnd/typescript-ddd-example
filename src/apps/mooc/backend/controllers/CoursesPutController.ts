ac4cd0e3439eb28921cb11b819add09ac8880f3a
jorgechavezrnd <jorgechavezrnd@gmail.com>
2021-12-20 18:28:52 -0400

    Add implementation for FileCourseRepository and integration test

--- src/apps/mooc/backend/controllers/CoursesPutController.ts
+++ src/apps/mooc/backend/controllers/CoursesPutController.ts
@@ -1,9 +1,16 @@
 import { Request, Response } from 'express';
 import httpStatus from 'http-status';
 import { Controller } from './Controller';
+import { CourseCreator } from '../../../../Contexts/Mooc/Courses/application/CourseCreator';
 
 export class CoursesPutController implements Controller {
+  constructor(private courseCreator: CourseCreator) {}
+
   async run(req: Request, res: Response): Promise<void> {
+    const {id, name, duration} = req.body;
+
+    await this.courseCreator.run(id, name, duration);
+
     res.status(httpStatus.CREATED).send();
   }
 }
