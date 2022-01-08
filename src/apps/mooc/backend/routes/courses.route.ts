6ee929a2a5479c8cff9d5babea8cc4d1b0e234a8
jorgechavezrnd <jorgechavezrnd@gmail.com>
2022-01-07 22:34:23 -0400

    Add validation for request. Also, add tests for the implementation of the validation for request

--- src/apps/mooc/backend/routes/courses.route.ts
+++ src/apps/mooc/backend/routes/courses.route.ts
@@ -1,7 +1,17 @@
 import { Router, Request, Response } from 'express';
 import container from '../dependency-injection';
+import { body } from "express-validator";
+import { validateReqSchema } from '.';
 
 export const register = (router: Router) => {
+  const reqSchema = [
+    body('id').exists().isString(),
+    body('name').exists().isString(),
+    body('duration').exists().isString()
+  ];
+
   const coursesPutController = container.get('Apps.mooc.controllers.CoursesPutController');
-  router.put('/courses/:id', (req: Request, res: Response) => coursesPutController.run(req, res));
+  router.put('/courses/:id', reqSchema, validateReqSchema, (req: Request, res: Response) =>
+    coursesPutController.run(req, res)
+  );
 };
