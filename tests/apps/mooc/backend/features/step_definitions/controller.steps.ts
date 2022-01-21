9b10790e4d371ae6d663c5d803db8a0d0e48d7a1
jorgechavezrnd <jorgechavezrnd@gmail.com>
2022-01-21 19:46:39 -0400

    Add call to arrange method of EnvironmentArranger after all tests in order to get a clean database for tests after run all tests

--- tests/apps/mooc/backend/features/step_definitions/controller.steps.ts
+++ tests/apps/mooc/backend/features/step_definitions/controller.steps.ts
@@ -34,6 +34,7 @@ BeforeAll(async () => {
 
 AfterAll(async () => {
   const environmentArranger: Promise<EnvironmentArranger> = container.get('Mooc.EnvironmentArranger');
+  await (await environmentArranger).arrange();
   await (await environmentArranger).close();
   await application.stop();
 });
