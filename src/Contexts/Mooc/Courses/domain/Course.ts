e0f823946db5bfcc8b35866e7f138606398a8850
jorgechavezrnd <jorgechavezrnd@gmail.com>
2022-01-20 22:35:26 -0400

    Add implementation of Course Repository for Mongo. Also, modify Course model in order to extend from AggregateRoot

--- src/Contexts/Mooc/Courses/domain/Course.ts
+++ src/Contexts/Mooc/Courses/domain/Course.ts
@@ -1,15 +1,37 @@
-import {CourseId} from '../../Shared/domain/Courses/CourseId';
-import {CourseName} from './CourseName';
-import {CourseDuration} from './CourseDuration';
+import { CourseId } from '../../Shared/domain/Courses/CourseId';
+import { CourseName } from './CourseName';
+import { CourseDuration } from './CourseDuration';
+import { AggregateRoot } from '../../../Shared/domain/AggregateRoot';
 
-export class Course {
+export class Course extends AggregateRoot {
   readonly id: CourseId;
   readonly name: CourseName;
   readonly duration: CourseDuration;
 
   constructor({ id, name, duration }: { id: CourseId; name: CourseName; duration: CourseDuration }) {
+    super();
     this.id = id;
     this.name = name;
     this.duration = duration;
+  }
+
+  static fromPrimitives(plainData: {
+    id: string;
+    name: string;
+    duration: string;
+  }): Course {
+    return new Course({
+      id: new CourseId(plainData.id),
+      name: new CourseName(plainData.name),
+      duration: new CourseDuration(plainData.duration)
+    });
+  }
+
+  toPrimitives(): any {
+    return {
+      id: this.id.value,
+      name: this.name.value,
+      duration: this.duration.value
+    };
   }
 }
